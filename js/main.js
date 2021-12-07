c566dea0613b1325baf452cae887a3106fcdd51a
jjunghoo <wjdgn2468@naver.com>
2021-12-07 21:34:39 +0900

    header in logo and sub-menu

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+    searchInputEl.focus();
+});
+
+/* focus되었을 때 */
+searchInputEl.addEventListener('focus', function () {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+//focus가 해제되었을 때
+searchInputEl.addEventListener('blur', function () {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
+
