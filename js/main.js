c4d1ce7b3fd45b893f07459d326c0b16198eb0cb
jjunghoo <wjdgn2468@naver.com>
2021-12-10 22:32:22 +0900

    section.awards in horizontal orientation slide function - use swiperjs

--- js/main.js
+++ js/main.js
@@ -62,9 +62,9 @@ new Swiper('.promotion .swiper-container', {
     spaceBetween: 10, //슬라이드 사이 여백
     centeredSlides: true, // 1번 슬라이드가 가운데 보이기
     loop: true,
-    // autoplay: {
-    //     delay: 5000
-    // }
+    autoplay: {
+        delay: 5000
+    },
     pagination: {
         el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
         clickable: true //사용자의 페이지 번호 요소 제어 가능 여부
@@ -74,7 +74,16 @@ new Swiper('.promotion .swiper-container', {
         nextEl: '.promotion .swiper-next'
     }
 });
-
+new Swiper('.awards .swiper-container', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,  // 한개의 슬래이드에 보여줄 이미지 갯수
+    // navigation: {
+    //     prevEl: '.awards .swiper-prev',
+    //     nextEl: '.awards .swiper-next'
+    // }
+});
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
